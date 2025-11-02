12b9f6764593af905d5b91e93815f4f3bc032820
FIROJ SARDAR <140225422+firojbabu452@users.noreply.github.com>
2025-11-02 22:49:23 +0530

    Fix parameter casing in docker_build function

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
-def call (String ProjectName,String ImageTag,String DockerHubUser){
+def call (String ProjectName,String imageTag,String DockerHubUser){
 sh "docker build -t ${DockerHubUser}/${ProjectName}:${imageTag} . "
 
 }
